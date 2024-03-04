fc80d90e397a99e2941e3d17762a47ae32e3045b
Liu-Jingzhe <91197315+Liu-Jingzhe@users.noreply.github.com>
2024-03-04 16:27:29 -0500

    Create run.sh

---
+++ mol/GCL/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd $(dirname $0)
+echo $(pwd)
+
+export PYTHONPATH="$(pwd)/../../../../":$PYTHONPATH
+experitment_time=$(date "+%Y-%m-%d-%H-%M-%S")
+save_path="/egr/research-dselab/liujin33/graph_scaling_law/models/mol/GCL/results"
+
+log_dir="$save_path/GNNGCL-mol-$experitment_time"
+mkdir -p $log_dir
+log_file="$log_dir/log.txt"
+checkpoint_dir="$log_dir/checkpoints"
+echo "Outputs redirected to $log_file"
+mkdir -p $checkpoint_dir
+python main.py > $log_file 2>&1
+wait
